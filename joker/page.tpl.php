1109c1814e8c40295a81cf95894559b977a10bcb
Josh <timmer.jo@gmail.com>
2015-06-24 13:57:54 -0600

    Hidden place holder items.

--- joker/page.tpl.php
+++ joker/page.tpl.php
@@ -128,10 +128,10 @@
                 <div id="upcoming-race" class="pure-u-1 pure-u-sm-3-5">
                     <div class="box sidebar-right">
                         <?php print render($page['content']) ?>
-                        <h2 class="remove-spacing">Upcoming Race Events</h2>
+                        <!--<h2 class="remove-spacing">Upcoming Race Events</h2>
                         <hr class="dashed">
                         <div class="pure-g"><!--Race event node-->
-                            <div class="pure-u-1-3">
+                            <!--<div class="pure-u-1-3">
                                 <a href="#"><img src="img/rcgears-small.png" alt="club picture" class="pure-img"></a>
                             </div>
                             <div class="pure-u-2-3">
@@ -144,7 +144,7 @@
                             </div>
 
                         </div>
-                        <hr>
+                        <hr>-->
                     </div>
                 </div>
                 <?php print render($page['sidebar_first']) ?>
